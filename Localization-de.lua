a8c6dbafb8e96a27a7ecd04307fca0ca3e25bd42
Phanx <addons@phanx.net>
2014-10-16 13:50:44 +0000

    - Add Russian translations - Clean up localization in general

--- Localization-de.lua
+++ Localization-de.lua
@@ -11,102 +11,104 @@
 ------------------------------------------------------------------------------
 
 if GetLocale() ~= "deDE" then return end
+local _, L = ...
 
 local EM_ON = "|cffffff00"
 local EM_OFF = "|r"
 local RED_ON = "|cffff4000"
 local RED_OFF = "|r"
 
-BADAPPLES_TEXT.TITLETEXT = "Badapples-Liste"
-BADAPPLES_TEXT.TAB_TOOLTIP = "Der Badapples-Liste anzeigen oder ändern."
+L.TITLETEXT = "Badapples-Liste"
+L.TAB_TOOLTIP = "Der Badapples-Liste anzeigen oder ändern."
 
-BADAPPLES_TEXT.COLUMN_NAME = "Name"
-BADAPPLES_TEXT.COLUMN_REASON = "Grund"
-BADAPPLES_TEXT.ADDED_BY_ON = "Hinzugefügt am %2$s von %1$s"
-BADAPPLES_TEXT.ADDED_BY = "Hinzugefügt von %s"
-BADAPPLES_TEXT.ADDED_ON = "Hinzugefügt am %s"
+L.COLUMN_NAME = "Name"
+L.COLUMN_REASON = "Grund"
+L.ADDED_BY_ON = "Hinzugefügt am %2$s von %1$s"
+L.ADDED_BY = "Hinzugefügt von %s"
+L.ADDED_ON = "Hinzugefügt am %s"
 
-BADAPPLES_TEXT.BUTTON_ADD = "Hinzufügen"
-BADAPPLES_TEXT.BUTTON_ADD_TITLE = "Spieler hinzufügen"
-BADAPPLES_TEXT.BUTTON_ADD_TOOLTIP = "Einen Spieler auf der Badapples-Liste hinzufügen."
-BADAPPLES_TEXT.BUTTON_REMOVE = "Entfernen"
-BADAPPLES_TEXT.BUTTON_REMOVE_TITLE = "Spieler entfernen"
-BADAPPLES_TEXT.BUTTON_REMOVE_TOOLTIP = "Den ausgewählte Spieiler von der Badapples-Liste entfernen."
-BADAPPLES_TEXT.BUTTON_SETCOLOR = "Farbe"
-BADAPPLES_TEXT.BUTTON_SETCOLOR_TITLE = "Farbe festlegen"
-BADAPPLES_TEXT.BUTTON_SETCOLOR_TOOLTIP = "Die Farbe festlegen, die verwendet wird, um die Namen der Spieler auf der Badapples-Liste zu hervorheben."
+L.BUTTON_ADD = "Hinzufügen"
+L.BUTTON_ADD_TITLE = "Spieler hinzufügen"
+L.BUTTON_ADD_TOOLTIP = "Einen Spieler auf der Badapples-Liste hinzufügen."
+L.BUTTON_REMOVE = "Entfernen"
+L.BUTTON_REMOVE_TITLE = "Spieler entfernen"
+L.BUTTON_REMOVE_TOOLTIP = "Den ausgewählte Spieiler von der Badapples-Liste entfernen."
+L.BUTTON_SETCOLOR = "Farbe"
+L.BUTTON_SETCOLOR_TITLE = "Farbe festlegen"
+L.BUTTON_SETCOLOR_TOOLTIP = "Die Farbe festlegen, die verwendet wird, um die Namen der Spieler auf der Badapples-Liste zu hervorheben."
 
-BADAPPLES_TEXT.TOTAL_EMPTY = "Badapples-Liste ist leer!"
-BADAPPLES_TEXT.TOTAL_SINGLE = "1 Badapple"
-BADAPPLES_TEXT.TOTAL_MULTIPLE = "%d Badapples"
+L.TOTAL_EMPTY = "Badapples-Liste ist leer!"
+L.TOTAL_SINGLE = "1 Badapple"
+L.TOTAL_MULTIPLE = "%d Badapples"
 
-BADAPPLES_TEXT.COMMAND_HELP = "hilfe"
-BADAPPLES_TEXT.COMMAND_LIST = "auflisten"
-BADAPPLES_TEXT.COMMAND_SHOW = "anziegen"
-BADAPPLES_TEXT.COMMAND_CHECK = "prüfen"
-BADAPPLES_TEXT.COMMAND_STATUS = "status"
-BADAPPLES_TEXT.COMMAND_ADD = "hinzufügen"
-BADAPPLES_TEXT.COMMAND_REMOVE = "entfernen"
-BADAPPLES_TEXT.COMMAND_REMOVEALL = "entfernenalle"
-BADAPPLES_TEXT.COMMAND_COLOR = "farbe"
-BADAPPLES_TEXT.COMMAND_SETCOLOR = "farbelegen"
-BADAPPLES_TEXT.COMMAND_REMOVEALL_CONFIRM = "bestätigen"
-BADAPPLES_TEXT.COMMAND_NOTAB = "keintab"
-BADAPPLES_TEXT.COMMAND_TOGGLETAB = "tab"
-BADAPPLES_TEXT.COMMAND_DEBUGON = "debugon"
-BADAPPLES_TEXT.COMMAND_DEBUGOFF = "debugoff"
+L.COMMAND = "/bad"
+L.COMMAND_HELP = "hilfe"
+L.COMMAND_LIST = "auflisten"
+L.COMMAND_SHOW = "anziegen"
+L.COMMAND_CHECK = "prüfen"
+L.COMMAND_STATUS = "status"
+L.COMMAND_ADD = "hinzufügen"
+L.COMMAND_REMOVE = "entfernen"
+L.COMMAND_REMOVEALL = "entfernenalle"
+L.COMMAND_COLOR = "farbe"
+L.COMMAND_SETCOLOR = "farbelegen"
+L.COMMAND_REMOVEALL_CONFIRM = "bestätigen"
+L.COMMAND_NOTAB = "keintab"
+L.COMMAND_TOGGLETAB = "tab"
+L.COMMAND_DEBUGON = "debugon"
+L.COMMAND_DEBUGOFF = "debugoff"
 
-BADAPPLES_TEXT.ADD_CONFIRM = EM_ON.."Der Spieler "..EM_OFF.."%s"..EM_ON.." auf der Badapples-Liste hinzugefügt: "..EM_OFF.."%s"
-BADAPPLES_TEXT.UPDATE_CONFIRM = EM_ON.."Der Grund des Spieler "..EM_OFF.."%s"..EM_ON.." aktualisiert: "..EM_OFF.."%s"
-BADAPPLES_TEXT.REMOVE_CONFIRM = EM_ON.."Der Spieler "..EM_OFF.."%s"..EM_ON.." von der Badapples-Liste entfernt."..EM_OFF
-BADAPPLES_TEXT.REMOVE_NOTFOUND = EM_ON.."Der Spieler "..EM_OFF.."%s"..EM_ON.." ist nicht auf der Badapples-Liste."..EM_OFF
-BADAPPLES_TEXT.PLAYERNAME_FAILED = EM_ON.."Man muss einen gültigen Namen eingeben."..EM_OFF
-BADAPPLES_TEXT.PLAYERNAME_ISSELF = EM_ON.."Man kann sich nicht selbst auf der Badapples-Liste hinzuzufügen!"..EM_OFF
-BADAPPLES_TEXT.LIST_FORMAT = "   %s"..EM_OFF..": %s"
-BADAPPLES_TEXT.STATUS_GOOD = EM_ON.."Der Spieler "..EM_OFF.."%s"..EM_ON.." ist NICHT auf der Badapples-Liste."
-BADAPPLES_TEXT.REMOVEALL_CONFIRM = EM_ON.."Alle Spieler von der Badapples-Liste entfernt."..EM_OFF
+L.ADD_CONFIRM = EM_ON.."Der Spieler "..EM_OFF.."%s"..EM_ON.." auf der Badapples-Liste hinzugefügt: "..EM_OFF.."%s"
+L.UPDATE_CONFIRM = EM_ON.."Der Grund des Spieler "..EM_OFF.."%s"..EM_ON.." aktualisiert: "..EM_OFF.."%s"
+L.REMOVE_CONFIRM = EM_ON.."Der Spieler "..EM_OFF.."%s"..EM_ON.." von der Badapples-Liste entfernt."..EM_OFF
+L.REMOVE_NOTFOUND = EM_ON.."Der Spieler "..EM_OFF.."%s"..EM_ON.." ist nicht auf der Badapples-Liste."..EM_OFF
+L.PLAYERNAME_FAILED = EM_ON.."Man muss einen gültigen Namen eingeben."..EM_OFF
+L.PLAYERNAME_ISSELF = EM_ON.."Man kann sich nicht selbst auf der Badapples-Liste hinzuzufügen!"..EM_OFF
+L.LIST_FORMAT = "   %s"..EM_OFF..": %s"
+L.STATUS_GOOD = EM_ON.."Der Spieler "..EM_OFF.."%s"..EM_ON.." ist NICHT auf der Badapples-Liste."
+L.REMOVEALL_CONFIRM = EM_ON.."Alle Spieler von der Badapples-Liste entfernt."..EM_OFF
 
-BADAPPLES_TEXT.PARTY_WARNING = "Gruppenmitglied %s ist auf der Badapples-Liste: %s"
-BADAPPLES_TEXT.PARTY_WARNING_NO_REASON = "Gruppenmitglied %s ist auf der Badapples-Liste."
-BADAPPLES_TEXT.PARTY_IGNORE_WARNING = "Gruppenmitglied %s wird ignoriert."
-BADAPPLES_TEXT.RAID_WARNING = "Schlachtzugsmitglied %s ist auf der Badapples-Liste: %s"
-BADAPPLES_TEXT.RAID_WARNING_NO_REASON = "Schlachtzugsmitglied %s ist auf der Badapples-Liste."
-BADAPPLES_TEXT.RAID_IGNORE_WARNING = "Schlachtzugsmitglied %s wird ignoriert."
-BADAPPLES_TEXT.NOTIFY_BAD = "Der Spieler %s ist auf der Badapples-Liste: %s"
-BADAPPLES_TEXT.NOTIFY_IGNORE = "Der Spieler %s wird ignoriert."
+L.PARTY_WARNING = "Gruppenmitglied %s ist auf der Badapples-Liste: %s"
+L.PARTY_WARNING_NO_REASON = "Gruppenmitglied %s ist auf der Badapples-Liste."
+L.PARTY_IGNORE_WARNING = "Gruppenmitglied %s wird ignoriert."
+L.RAID_WARNING = "Schlachtzugsmitglied %s ist auf der Badapples-Liste: %s"
+L.RAID_WARNING_NO_REASON = "Schlachtzugsmitglied %s ist auf der Badapples-Liste."
+L.RAID_IGNORE_WARNING = "Schlachtzugsmitglied %s wird ignoriert."
+L.NOTIFY_BAD = "Der Spieler %s ist auf der Badapples-Liste: %s"
+L.NOTIFY_IGNORE = "Der Spieler %s wird ignoriert."
 
-BADAPPLES_TEXT.NO_REASON = "(keinen Grund)"
-BADAPPLES_TEXT.PARTY_INVITE_TEXT = "Badapple %s hat Euch in eine Gruppe eingeladen."
-BADAPPLES_TEXT.PARTY_IGNORE_INVITE_TEXT = "Der ignorierte Spieler %s hat Euch in eine Gruppe eingeladen."
-BADAPPLES_TEXT.PARTY_INVITE_BUTTON = "Dennoch akzeptieren"
-BADAPPLES_TEXT.INVITE_TEXT = "%s ist auf der Badapples-Liste. Dennoch einladen?"
-BADAPPLES_TEXT.INVITE_IGNORE_TEXT = "%s wird ignoriert. Dennoch einladen?"
-BADAPPLES_TEXT.PLAYER_ADD_TEXT = "Den Namen eines Spielers eingeben, um auf der Badapples-Liste hinzuzufügen:"
-BADAPPLES_TEXT.PLAYER_ADD_CONFIRM_TEXT = "%s auf der Badapples-Liste hinzufügen?"
-BADAPPLES_TEXT.PLAYER_REMOVE_CONFIRM_TEXT = "%s von der Badapples-Liste entfernen?"
-BADAPPLES_TEXT.REMOVEALL_CONFIRM_TEXT = "Das werden alle Spieler von der Badapples-Liste zu entfernen. Fortfahren?"
-BADAPPLES_TEXT.DISABLE_TAB = "Deaktivieren"
-BADAPPLES_TEXT.TOGGLE_TAB = "Tab ein/aus"
-BADAPPLES_TEXT.TAB_CONFIRM = EM_ON.."Badapples-Tab ist %s"..EM_OFF
+L.NO_REASON = "(keinen Grund)"
+L.PARTY_INVITE_TEXT = "Badapple %s hat Euch in eine Gruppe eingeladen."
+L.PARTY_IGNORE_INVITE_TEXT = "Der ignorierte Spieler %s hat Euch in eine Gruppe eingeladen."
+L.PARTY_INVITE_BUTTON = "Dennoch akzeptieren"
+L.INVITE_TEXT = "%s ist auf der Badapples-Liste. Dennoch einladen?"
+L.INVITE_IGNORE_TEXT = "%s wird ignoriert. Dennoch einladen?"
+L.PLAYER_ADD_TEXT = "Den Namen eines Spielers eingeben, um auf der Badapples-Liste hinzuzufügen:"
+L.PLAYER_ADD_CONFIRM_TEXT = "%s auf der Badapples-Liste hinzufügen?"
+L.PLAYER_REMOVE_CONFIRM_TEXT = "%s von der Badapples-Liste entfernen?"
+L.REMOVEALL_CONFIRM_TEXT = "Das werden alle Spieler von der Badapples-Liste zu entfernen. Fortfahren?"
+L.DISABLE_TAB = "Deaktivieren"
+L.TOGGLE_TAB = "Tab ein/aus"
+L.TAB_CONFIRM = EM_ON.."Badapples-Tab ist %s"..EM_OFF
 
-BADAPPLES_TEXT.DEBUGON_CONFIRM = "Badapples-Fehlerbeseitigung aktiviert."
-BADAPPLES_TEXT.DEBUGOFF_CONFIRM = "Badapples-Fehlerbeseitigung deaktiviert."
+L.DEBUGON_CONFIRM = "Badapples-Fehlerbeseitigung aktiviert."
+L.DEBUGOFF_CONFIRM = "Badapples-Fehlerbeseitigung deaktiviert."
 
-BADAPPLES_TEXT.REMOVEALL_WARNING = RED_ON.."ACHTUNG: Alle Spieler werden von der Badapples-Liste entfernt."..RED_OFF.."\n"..EM_ON.."Gib "..EM_OFF.."/badapples "..BADAPPLES_TEXT.COMMAND_REMOVEALL.." "..BADAPPLES_TEXT.COMMAND_REMOVEALL_CONFIRM..EM_ON.." ein, zu fortfahren."..EM_OFF
+L.REMOVEALL_WARNING = RED_ON.."ACHTUNG: Alle Spieler werden von der Badapples-Liste entfernt."..RED_OFF.."\n"..EM_ON.."Gib "..EM_OFF.."/badapples "..L.COMMAND_REMOVEALL.." "..L.COMMAND_REMOVEALL_CONFIRM..EM_ON.." ein, zu fortfahren."..EM_OFF
 
-BADAPPLES_DESCRIPTION = "Ermöglichen, eine Liste der Namen der Spieler halten, die man lieber nicht mit zu spielen."
+L.DESCRIPTION = "Ermöglichen, eine Liste der Namen der Spieler halten, die man lieber nicht mit zu spielen."
 
-BADAPPLES_HELP = {
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_HELP..EM_OFF.." um eine Hilfe-Meldung anziegen",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_LIST..EM_OFF.." um die Badapples-Liste im Fenster anzeigen (kann lang sein)",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_SHOW..EM_OFF.." um den Badapples-Fenster anzeigen",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_ADD.." <Spielername> [Grund]"..EM_OFF.." um einen Namen bei optionalen Grund hinzufügen",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_REMOVE.." <Spielername>"..EM_OFF.." um einen Namen entfernen",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_CHECK.." <Spielername>"..EM_OFF.." um den Status eines Names überprüfen",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_REMOVEALL..EM_OFF.." um alle Spieler entfernen",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_COLOR..EM_OFF.." um die Hervorhebungsfarbe festlegen",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_TOGGLETAB..EM_OFF.." um die Platzierung des Tab umschalten",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_NOTAB..EM_OFF.." um den Tab deaktivieren",
+L.HELP = {
+	EM_ON..L.COMMAND.." "..L.COMMAND_HELP..EM_OFF.." um eine Hilfe-Meldung anziegen",
+	EM_ON..L.COMMAND.." "..L.COMMAND_LIST..EM_OFF.." um die Badapples-Liste im Fenster anzeigen (kann lang sein)",
+	EM_ON..L.COMMAND.." "..L.COMMAND_SHOW..EM_OFF.." um den Badapples-Fenster anzeigen",
+	EM_ON..L.COMMAND.." "..L.COMMAND_ADD.." <Spielername> [Grund]"..EM_OFF.." um einen Namen bei optionalen Grund hinzufügen",
+	EM_ON..L.COMMAND.." "..L.COMMAND_REMOVE.." <Spielername>"..EM_OFF.." um einen Namen entfernen",
+	EM_ON..L.COMMAND.." "..L.COMMAND_CHECK.." <Spielername>"..EM_OFF.." um den Status eines Names überprüfen",
+	EM_ON..L.COMMAND.." "..L.COMMAND_REMOVEALL..EM_OFF.." um alle Spieler entfernen",
+	EM_ON..L.COMMAND.." "..L.COMMAND_COLOR..EM_OFF.." um die Hervorhebungsfarbe festlegen",
+	EM_ON..L.COMMAND.." "..L.COMMAND_TOGGLETAB..EM_OFF.." um die Platzierung des Tab umschalten",
+	EM_ON..L.COMMAND.." "..L.COMMAND_NOTAB..EM_OFF.." um den Tab deaktivieren",
 	"",
 	"Man kann auch "..EM_ON.."/badapples"..EM_OFF.." statt "..EM_ON.."/bad"..EM_OFF.." eingeben, mit diesen Befehlen.",
 }
--- Localization-es.lua
+++ Localization-es.lua
@@ -11,112 +11,114 @@
 ------------------------------------------------------------------------------
 
 if strsub(GetLocale(), 1, 2) ~= "es" then return end
+local _, L = ...
 
 local EM_ON = "|cffffff00"
 local EM_OFF = "|r"
 local RED_ON = "|cffff4000"
 local RED_OFF = "|r"
 
-BADAPPLES_TEXT.TITLETEXT = "Lista de Badapples"
-BADAPPLES_TEXT.TAB_TOOLTIP = "Ver o modificar la lista de Badapples."
+L.TITLETEXT = "Lista de Badapples"
+L.TAB_TOOLTIP = "Ver o modificar la lista de Badapples."
 
-BADAPPLES_TEXT.COLUMN_NAME = "Nombre"
-BADAPPLES_TEXT.COLUMN_REASON = "Rázon"
-BADAPPLES_TEXT.ADDED_BY_ON = "Añadido el %s por %s"
-BADAPPLES_TEXT.ADDED_BY = "Added por %s"
-BADAPPLES_TEXT.ADDED_ON = "Added el %s"
+L.COLUMN_NAME = "Nombre"
+L.COLUMN_REASON = "Rázon"
+L.ADDED_BY_ON = "Añadido el %s por %s"
+L.ADDED_BY = "Added por %s"
+L.ADDED_ON = "Added el %s"
 
-BADAPPLES_TEXT.BUTTON_ADD = "Add"
-BADAPPLES_TEXT.BUTTON_ADD_TITLE = "Add Player"
-BADAPPLES_TEXT.BUTTON_ADD_TOOLTIP = "Adds a player to your Badapples list."
-BADAPPLES_TEXT.BUTTON_REMOVE = "Remove"
-BADAPPLES_TEXT.BUTTON_REMOVE_TITLE = "Remove Player"
-BADAPPLES_TEXT.BUTTON_REMOVE_TOOLTIP = "Removes the selected player from your Badapples list."
-BADAPPLES_TEXT.BUTTON_SETCOLOR = "Set Color"
-BADAPPLES_TEXT.BUTTON_SETCOLOR_TITLE = "Set Color"
-BADAPPLES_TEXT.BUTTON_SETCOLOR_TOOLTIP = "Change the color that is used to highlight Badapple players in chat, target, and tooltip."
+L.BUTTON_ADD = "Add"
+L.BUTTON_ADD_TITLE = "Add Player"
+L.BUTTON_ADD_TOOLTIP = "Adds a player to your Badapples list."
+L.BUTTON_REMOVE = "Remove"
+L.BUTTON_REMOVE_TITLE = "Remove Player"
+L.BUTTON_REMOVE_TOOLTIP = "Removes the selected player from your Badapples list."
+L.BUTTON_SETCOLOR = "Set Color"
+L.BUTTON_SETCOLOR_TITLE = "Set Color"
+L.BUTTON_SETCOLOR_TOOLTIP = "Change the color that is used to highlight Badapple players in chat, target, and tooltip."
 
-BADAPPLES_TEXT.TOTAL_EMPTY = "Badapples list is empty!"
-BADAPPLES_TEXT.TOTAL_SINGLE = "1 Badapple"
-BADAPPLES_TEXT.TOTAL_MULTIPLE = "%d Badapples"
+L.TOTAL_EMPTY = "Badapples list is empty!"
+L.TOTAL_SINGLE = "1 Badapple"
+L.TOTAL_MULTIPLE = "%d Badapples"
 
-BADAPPLES_TEXT.BUTTON_ADD = "Añadir"
-BADAPPLES_TEXT.BUTTON_ADD_TITLE = "Añadir un jugador"
-BADAPPLES_TEXT.BUTTON_ADD_TOOLTIP = "Añadir un jugador a la lista de Badapples."
-BADAPPLES_TEXT.BUTTON_REMOVE = "Eliminar"
-BADAPPLES_TEXT.BUTTON_REMOVE_TITLE = "Eliminar un jugador"
-BADAPPLES_TEXT.BUTTON_REMOVE_TOOLTIP = "Eliminar el jugador seleccionado de la lista de Badapples."
-BADAPPLES_TEXT.BUTTON_SETCOLOR = "Color"
-BADAPPLES_TEXT.BUTTON_SETCOLOR_TITLE = "Cambiar el color"
-BADAPPLES_TEXT.BUTTON_SETCOLOR_TOOLTIP = "Cambiar el color utilizado para resaltar los nombres de los jugadores en la lista de los jugadores en la lista de Badapples."
+L.BUTTON_ADD = "Añadir"
+L.BUTTON_ADD_TITLE = "Añadir un jugador"
+L.BUTTON_ADD_TOOLTIP = "Añadir un jugador a la lista de Badapples."
+L.BUTTON_REMOVE = "Eliminar"
+L.BUTTON_REMOVE_TITLE = "Eliminar un jugador"
+L.BUTTON_REMOVE_TOOLTIP = "Eliminar el jugador seleccionado de la lista de Badapples."
+L.BUTTON_SETCOLOR = "Color"
+L.BUTTON_SETCOLOR_TITLE = "Cambiar el color"
+L.BUTTON_SETCOLOR_TOOLTIP = "Cambiar el color utilizado para resaltar los nombres de los jugadores en la lista de los jugadores en la lista de Badapples."
 
-BADAPPLES_TEXT.COMMAND_HELP = "ayuda"
-BADAPPLES_TEXT.COMMAND_LIST = "lista"
-BADAPPLES_TEXT.COMMAND_SHOW = "mostra"
-BADAPPLES_TEXT.COMMAND_CHECK = "prueba"
-BADAPPLES_TEXT.COMMAND_STATUS = "estado"
-BADAPPLES_TEXT.COMMAND_ADD = "añade"
-BADAPPLES_TEXT.COMMAND_REMOVE = "quite"
-BADAPPLES_TEXT.COMMAND_REMOVEALL = "quitetodos"
-BADAPPLES_TEXT.COMMAND_COLOR = "color"
-BADAPPLES_TEXT.COMMAND_SETCOLOR = "cambiacolor"
-BADAPPLES_TEXT.COMMAND_REMOVEALL_CONFIRM = "confirma"
-BADAPPLES_TEXT.COMMAND_NOTAB = "nopestaña"
-BADAPPLES_TEXT.COMMAND_TOGGLETAB = "pestaña"
-BADAPPLES_TEXT.COMMAND_DEBUGON = "debugon"
-BADAPPLES_TEXT.COMMAND_DEBUGOFF = "debugoff"
+L.COMMAND = "/bad"
+L.COMMAND_HELP = "ayuda"
+L.COMMAND_LIST = "lista"
+L.COMMAND_SHOW = "mostra"
+L.COMMAND_CHECK = "prueba"
+L.COMMAND_STATUS = "estado"
+L.COMMAND_ADD = "añade"
+L.COMMAND_REMOVE = "quite"
+L.COMMAND_REMOVEALL = "quitetodos"
+L.COMMAND_COLOR = "color"
+L.COMMAND_SETCOLOR = "cambiacolor"
+L.COMMAND_REMOVEALL_CONFIRM = "confirma"
+L.COMMAND_NOTAB = "nopestaña"
+L.COMMAND_TOGGLETAB = "pestaña"
+L.COMMAND_DEBUGON = "debugon"
+L.COMMAND_DEBUGOFF = "debugoff"
 
-BADAPPLES_TEXT.ADD_CONFIRM = EM_ON.."Jugador "..EM_OFF.."%s"..EM_ON.." añadido a la lista de Badapples: "..EM_OFF.."%s"
-BADAPPLES_TEXT.UPDATE_CONFIRM = EM_ON.."Jugador "..EM_OFF.."%s"..EM_ON.." rázon actualizado: "..EM_OFF.."%s"
-BADAPPLES_TEXT.REMOVE_CONFIRM = EM_ON.."Jugador "..EM_OFF.."%s"..EM_ON.." eliminado de la lista de Badapples."..EM_OFF
-BADAPPLES_TEXT.REMOVE_NOTFOUND = EM_ON.."Jugador "..EM_OFF.."%s"..EM_ON.." no está en la lista de Badapples."..EM_OFF
-BADAPPLES_TEXT.PLAYERNAME_FAILED = EM_ON.."Debes introducir un nombre válido."..EM_OFF
-BADAPPLES_TEXT.PLAYERNAME_ISSELF = EM_ON.."¡No puedes añadir ti mismo a la lista!"..EM_OFF
-BADAPPLES_TEXT.LIST_FORMAT = "   %s"..EM_OFF..": %s"
-BADAPPLES_TEXT.STATUS_GOOD = EM_ON.."Jugador "..EM_OFF.."%s"..EM_ON.." NO ESTÁ en la lista de Badapples."
-BADAPPLES_TEXT.REMOVEALL_CONFIRM = EM_ON.."Todos jugadores eliminado de la lista de Badapples."..EM_OFF
+L.ADD_CONFIRM = EM_ON.."Jugador "..EM_OFF.."%s"..EM_ON.." añadido a la lista de Badapples: "..EM_OFF.."%s"
+L.UPDATE_CONFIRM = EM_ON.."Jugador "..EM_OFF.."%s"..EM_ON.." rázon actualizado: "..EM_OFF.."%s"
+L.REMOVE_CONFIRM = EM_ON.."Jugador "..EM_OFF.."%s"..EM_ON.." eliminado de la lista de Badapples."..EM_OFF
+L.REMOVE_NOTFOUND = EM_ON.."Jugador "..EM_OFF.."%s"..EM_ON.." no está en la lista de Badapples."..EM_OFF
+L.PLAYERNAME_FAILED = EM_ON.."Debes introducir un nombre válido."..EM_OFF
+L.PLAYERNAME_ISSELF = EM_ON.."¡No puedes añadir ti mismo a la lista!"..EM_OFF
+L.LIST_FORMAT = "   %s"..EM_OFF..": %s"
+L.STATUS_GOOD = EM_ON.."Jugador "..EM_OFF.."%s"..EM_ON.." NO ESTÁ en la lista de Badapples."
+L.REMOVEALL_CONFIRM = EM_ON.."Todos jugadores eliminado de la lista de Badapples."..EM_OFF
 
-BADAPPLES_TEXT.PARTY_WARNING = "El miembro del grupo %s está en la lista de Badapples: %s"
-BADAPPLES_TEXT.PARTY_WARNING_NO_REASON = "El miembro del grupo %s está en la lista de Badapples."
-BADAPPLES_TEXT.PARTY_IGNORE_WARNING = "El miembro del grupo %s está en la lista de ignorados."
-BADAPPLES_TEXT.RAID_WARNING = "El miembro de la banda %s está en la lista de Badapples: %s"
-BADAPPLES_TEXT.RAID_WARNING_NO_REASON = "El miembro de la banda %s está en la lista de Badapples."
-BADAPPLES_TEXT.RAID_IGNORE_WARNING = "El miembro de la banda %s está en la lista de ignorados."
-BADAPPLES_TEXT.NOTIFY_BAD = "Jugador %s está en la lista de Badapples: %s"
-BADAPPLES_TEXT.NOTIFY_IGNORE = "Jugador %s está en la lista de ignorados."
+L.PARTY_WARNING = "El miembro del grupo %s está en la lista de Badapples: %s"
+L.PARTY_WARNING_NO_REASON = "El miembro del grupo %s está en la lista de Badapples."
+L.PARTY_IGNORE_WARNING = "El miembro del grupo %s está en la lista de ignorados."
+L.RAID_WARNING = "El miembro de la banda %s está en la lista de Badapples: %s"
+L.RAID_WARNING_NO_REASON = "El miembro de la banda %s está en la lista de Badapples."
+L.RAID_IGNORE_WARNING = "El miembro de la banda %s está en la lista de ignorados."
+L.NOTIFY_BAD = "Jugador %s está en la lista de Badapples: %s"
+L.NOTIFY_IGNORE = "Jugador %s está en la lista de ignorados."
 
-BADAPPLES_TEXT.NO_REASON = "(sin rázon)"
-BADAPPLES_TEXT.PARTY_INVITE_TEXT = "El jugador Badapple %s te ha invitado a un grupo."
-BADAPPLES_TEXT.PARTY_IGNORE_INVITE_TEXT = "El jugador ignorado %s te ha invitado a un grupo."
-BADAPPLES_TEXT.PARTY_INVITE_BUTTON = "Acceptar"
-BADAPPLES_TEXT.INVITE_TEXT = "%s está en la lista de Badapples. ¿Invitar anyway?"
-BADAPPLES_TEXT.INVITE_IGNORE_TEXT = "%s está en la lista de ignorados. ¿Invitar anyway?"
-BADAPPLES_TEXT.PLAYER_ADD_TEXT = "Introducir el nombre de un jugador para añadir a la lista:"
-BADAPPLES_TEXT.PLAYER_ADD_CONFIRM_TEXT = "¿Añadir %s a la lista de Badapples?"
-BADAPPLES_TEXT.PLAYER_REMOVE_CONFIRM_TEXT = "¿Eliminar %s de la lista de Badapples?"
-BADAPPLES_TEXT.REMOVEALL_CONFIRM_TEXT = "Todos los jugadores serán removidos de la lista de Badapples. ¿Estás seguro que deseas continuar?"
-BADAPPLES_TEXT.DISABLE_TAB = "Desactivado"
-BADAPPLES_TEXT.TOGGLE_TAB = "Alternar"
-BADAPPLES_TEXT.TAB_CONFIRM = EM_ON.."Badapples pestaña está %s"..EM_OFF
+L.NO_REASON = "(sin rázon)"
+L.PARTY_INVITE_TEXT = "El jugador Badapple %s te ha invitado a un grupo."
+L.PARTY_IGNORE_INVITE_TEXT = "El jugador ignorado %s te ha invitado a un grupo."
+L.PARTY_INVITE_BUTTON = "Acceptar"
+L.INVITE_TEXT = "%s está en la lista de Badapples. ¿Invitar anyway?"
+L.INVITE_IGNORE_TEXT = "%s está en la lista de ignorados. ¿Invitar anyway?"
+L.PLAYER_ADD_TEXT = "Introducir el nombre de un jugador para añadir a la lista:"
+L.PLAYER_ADD_CONFIRM_TEXT = "¿Añadir %s a la lista de Badapples?"
+L.PLAYER_REMOVE_CONFIRM_TEXT = "¿Eliminar %s de la lista de Badapples?"
+L.REMOVEALL_CONFIRM_TEXT = "Todos los jugadores serán removidos de la lista de Badapples. ¿Estás seguro que deseas continuar?"
+L.DISABLE_TAB = "Desactivado"
+L.TOGGLE_TAB = "Alternar"
+L.TAB_CONFIRM = EM_ON.."Badapples pestaña está %s"..EM_OFF
 
-BADAPPLES_TEXT.DEBUGON_CONFIRM = "Depuración de Badapples está activado."
-BADAPPLES_TEXT.DEBUGOFF_CONFIRM = "Depuración de Badapples está desactivado."
+L.DEBUGON_CONFIRM = "Depuración de Badapples está activado."
+L.DEBUGOFF_CONFIRM = "Depuración de Badapples está desactivado."
 
-BADAPPLES_TEXT.REMOVEALL_WARNING = RED_ON.."AVISO: Todos los jugadores serán removidos de la lista de Badapples."..RED_OFF.."\n"..EM_ON.."Usa "..EM_OFF.."/badapples "..BADAPPLES_TEXT.COMMAND_REMOVEALL.." "..BADAPPLES_TEXT.COMMAND_REMOVEALL_CONFIRM..EM_ON.." para continuar"..EM_OFF
+L.REMOVEALL_WARNING = RED_ON.."AVISO: Todos los jugadores serán removidos de la lista de Badapples."..RED_OFF.."\n"..EM_ON.."Usa "..EM_OFF.."/badapples "..L.COMMAND_REMOVEALL.." "..L.COMMAND_REMOVEALL_CONFIRM..EM_ON.." para continuar"..EM_OFF
 
-BADAPPLES_DESCRIPTION = "Te permite añadir los nombres (con razones opcional) a una lista de \"manzanas malas\", o jugadores con los que deseas recordar a evitar."
+L.DESCRIPTION = "Te permite añadir los nombres (con razones opcional) a una lista de \"manzanas malas\", o jugadores con los que deseas recordar a evitar."
 
-BADAPPLES_HELP = {
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_HELP..EM_OFF.." para mostrar esta mensaje de ayuda",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_LIST..EM_OFF.." para mostrar la lista de Badapples en la ventana de chat (puede ser largo)",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_SHOW..EM_OFF.." para mostrar el marco de la lista de Badapples",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_ADD.." <nombre> [rázon]"..EM_OFF.." para añadir un jugador y una rázon opcional",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_REMOVE.." <nombre>"..EM_OFF.." para eliminar un jugador",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_CHECK.." <nombre>"..EM_OFF.." para probar el estado de un jugador",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_REMOVEALL..EM_OFF.." para eliminar todos jugadores",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_COLOR..EM_OFF.." para establecer el color de resaltado de Badapples",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_TOGGLETAB..EM_OFF.." para mover la pestaña de Badapples",
-	EM_ON.."/bad "..BADAPPLES_TEXT.COMMAND_NOTAB..EM_OFF.." para desactivar la pestaña de Badapples",
+L.HELP = {
+	EM_ON..L.COMMAND.." "..L.COMMAND_HELP..EM_OFF.." para mostrar esta mensaje de ayuda",
+	EM_ON..L.COMMAND.." "..L.COMMAND_LIST..EM_OFF.." para mostrar la lista de Badapples en la ventana de chat (puede ser largo)",
+	EM_ON..L.COMMAND.." "..L.COMMAND_SHOW..EM_OFF.." para mostrar el marco de la lista de Badapples",
+	EM_ON..L.COMMAND.." "..L.COMMAND_ADD.." <nombre> [rázon]"..EM_OFF.." para añadir un jugador y una rázon opcional",
+	EM_ON..L.COMMAND.." "..L.COMMAND_REMOVE.." <nombre>"..EM_OFF.." para eliminar un jugador",
+	EM_ON..L.COMMAND.." "..L.COMMAND_CHECK.." <nombre>"..EM_OFF.." para probar el estado de un jugador",
+	EM_ON..L.COMMAND.." "..L.COMMAND_REMOVEALL..EM_OFF.." para eliminar todos jugadores",
+	EM_ON..L.COMMAND.." "..L.COMMAND_COLOR..EM_OFF.." para establecer el color de resaltado de Badapples",
+	EM_ON..L.COMMAND.." "..L.COMMAND_TOGGLETAB..EM_OFF.." para mover la pestaña de Badapples",
+	EM_ON..L.COMMAND.." "..L.COMMAND_NOTAB..EM_OFF.." para desactivar la pestaña de Badapples",
 	"",
 	"También puedes utilizar "..EM_ON.."/badapples"..EM_OFF.." en lugar de "..EM_ON.."/bad"..EM_OFF.." para los comandos enumerados anteriormente.",
 }
